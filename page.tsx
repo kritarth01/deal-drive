83d24c681659b37814cfcfe0ed41b4a64a0a1205
kritarth mishra <113338712+kritarth01@users.noreply.github.com>
2024-06-27 02:49:07 +0530

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,56 @@
+import HeroCarousel from "@/components/HeroCarousel";
+import Searchbar from "@/components/Searchbar";
+import Image from "next/image";
+import { getAllProducts } from "@/lib/actions";
+import ProductCard from "@/components/ProductCard";
+import { revalidatePath } from "next/cache";
+const Home = async () => {
+  const allProducts = await getAllProducts();
+  revalidatePath('/');
+  revalidatePath('/products');
+  return (
+    <>
+      <section className="px-6 md:px-20 py-24">
+        <div className="flex max-xl:flex-col gap-16">
+          <div className="flex flex-col justify-center">
+            <p className="small-text">
+              Start Smart Shopping Today:
+              <Image
+                src="/assets/icons/arrow-right.svg"
+                alt="arrow-right"
+                width={16}
+                height={16}
+              />
+            </p>
+
+            <h1 className="head-text">
+              Supercharge Your Savings Journey with
+              <span className="text-primary"> Deal Diver!</span>
+            </h1>
+
+            <p className="mt-6">
+              Utilize Our Self-Service Product and Growth Analytics to Enhance
+              Conversions, Engagement, and Customer Retention.
+            </p>
+
+            <Searchbar />
+          </div>
+
+          <HeroCarousel />
+        </div>
+      </section>
+
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
+
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+          {allProducts?.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
